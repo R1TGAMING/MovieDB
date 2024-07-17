601247b59e2f72fbdea711fac6984120c1acd062
RTdev <77839017+R1TGAMING@users.noreply.github.com>
2024-07-17 13:05:29 +0000

    Mapping The API

--- src/components/Pages/main.jsx
+++ src/components/Pages/main.jsx
@@ -2,21 +2,31 @@ import {useState, useEffect} from "react"
 import axios from 'axios'
 
 export default function Main() {
+  // Create a state variable for the data
   const [data, setData] = useState([])
 
+  // Get the api key from env
+  const api_key = proccess.env.API_KEY
   
   useEffect(() => {
-    const api_key = process.env.API_KEY   
-     axios.get("https://api.themoviedb.org/3/movie/popular?api_key=" + api_key).then(response => {
-  console.log(response.data)
-  setData(response)
-}, [])
+    //Get the API data and set it to the state
+axios.get("https://api.themoviedb.org/3/movie/popular?api_key=" + api_key)
+    .then(res => {
+      setData(res.data.results)
+    })
+    .catch(err => {
+      console.log(err)
+    })
     
-  })
-  console.log(data)
+  }, [])
+  
   return (
     <main>
-      
+      {data.map((item) => {
+      return (
+        <p>{item.title}</p>
+      )
+      })}
       
     </main>
   );
